41ea98209f24a50617c906e1622c504d1c1a5bdb
BFish <95540734+bfish1996@users.noreply.github.com>
2024-05-07 16:36:54 +0000

    url add and send to apify

---
+++ url_add.py
@@ -0,0 +1,14 @@
+#Create a loop, adding URLs to an array for now. 
+#Future, for a database with fields like "done" etc... 
+#Do you want to add another?
+
+url = []
+
+while True:
+    url.append (input("Add your URL"))
+    add_another = input("Add another?")
+    if add_another == "T":
+        continue
+    else:
+        print(url)
+        exit()
